d6a259d80d4d88208663eca293fbde0fe9b4a011
Mauricio Carbal Martinez <mauricio.carbal@clovit.com>
2025-10-30 20:59:31 -0500

    borrado de contraseña

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,6 +4,6 @@ export default registerAs('database', () => ({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432', 10),
   user: process.env.DB_USER || 'postgres',
-  pass: process.env.DB_PASS ?? 'CAM2018',
+  pass: process.env.DB_PASS ?? '',
   name: process.env.DB_NAME || 'inventory_db',
 }));
